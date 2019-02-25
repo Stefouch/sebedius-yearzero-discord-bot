6c4d7013f2ba14d3dfd3f0a406c622b96453ea4a
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-02-25 17:43:00 +0100

    simplified Nd6 replacer, code cleansing
    1.2.2

--- commands/scrap.js
+++ commands/scrap.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Config = require('../config.json');
 const YZEmbed = require('../utils/YZEmbed.js');
-const { rand } = require('../utils/utils.js');
+const { rand, rollD6 } = require('../utils/utils.js');
 
 // Loading the available scrap.
 let scrapList;
@@ -22,34 +22,27 @@ module.exports = {
 	args: false,
 	usage: '[quantity]',
 	execute(args, message) {
-		let text = '';
+		let desc = '';
 		let qty = 1;
 
 		if (/^\d{1,2}$/.test(args[0])) {
-			qty = Math.min(Number(args[0]), Config.commands.scrap.max);
+			qty = Math.min(+args[0], Config.commands.scrap.max);
 		}
 
 		for (let i = 0; i < qty; i++) {
 			const roll = rand(1, scrapList.length) - 1;
 			const scrap = scrapList[roll];
 
-			text += `\n– ${scrap}`;
+			desc += `\n– ${scrap}`;
 		}
 		// Dice replacements.
-		text = text.replace(/D666/gmi, rand(1, 6) * 100 + rand(1, 6) * 10 + rand(1, 6));
-		text = text.replace(/D66/gmi, rand(1, 6) * 10 + rand(1, 6));
-		text = text.replace(/(\d)D6/gmi, (correspondance, p1, decalage, chaine) => {
-			// Help: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/replace#Indiquer_une_fonction_comme_param%C3%A8tre
-			const nbre = Number(p1);
-			let roll = 0;
-			for (let i = 0; i < nbre; i++) {
-				roll += rand(1, 6);
-			}
-			return roll;
-		});
-		text = text.replace(/D6/gmi, rand(1, 6));
+		desc = desc.replace(/D666/gmi, rand(1, 6) * 100 + rand(1, 6) * 10 + rand(1, 6));
+		desc = desc.replace(/D66/gmi, rand(1, 6) * 10 + rand(1, 6));
+		desc = desc.replace(/(\d)D6/gmi, (match, p1) => rollD6(p1));
+		desc = desc.replace(/D6/gmi, rand(1, 6));
 
-		const embed = new YZEmbed(`Scrap Item${(qty > 1) ? 's' : ''} Found`, text, message);
+		const title = `Scrap Item${(qty > 1) ? 's' : ''} Found`;
+		const embed = new YZEmbed(title, desc, message, true);
 
 		message.channel.send(embed);
 	},
