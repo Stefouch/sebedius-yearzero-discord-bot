260989cc3b35b5b797bce6c2764a28de44b52b84
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-02-25 17:44:23 +0100

    changed json keys, moved fn strLcFirst() to Utils, code cleansing
    1.2.2

--- commands/rumor.js
+++ commands/rumor.js
@@ -1,43 +1,39 @@
 const Rumors = require('../sheets/rumors.json');
 const YZEmbed = require('../utils/YZEmbed.js');
-const { rand } = require('../utils/utils.js');
+const { rand, strLcFirst } = require('../utils/utils.js');
 
 module.exports = {
 	name: 'rumor',
 	description: 'Tells a random rumor.',
-	// aliases: ['mut'],
 	guildOnly: false,
 	args: false,
-	// usage: '',
 	execute(args, message) {
-		const nb = rand(1, Rumors.myz.length) - 1;
-		const rumorObject = Rumors.myz[nb];
+		const nb = rand(1, Rumors.stories.length) - 1;
+		const rumorStory = Rumors.stories[nb];
 
-		// RumorObject is an Object with:
-		// rumor.start: string
-		// rumor.ends: Array<string> (multiple ends)
-		const rumorStart = rumorObject.start;
-		const nbEnd = rand(1, rumorObject.ends.length) - 1;
-		const rumorEnd = rumorObject.ends[nbEnd];
+		// RumorStory is an Object with:
+		// rumor.head: string
+		// rumor.bodies: Array<string> (multiple possible ends)
+		const rumorHead = rumorStory.head;
+		const nbBody = rand(1, rumorStory.bodies.length) - 1;
+		const rumorBody = rumorStory.bodies[nbBody];
+		let rumorText = `${rumorHead} ${strLcFirst(rumorBody)}`;
 
-		let rumorText = rumorStart + ' ' + strLcFirst(rumorEnd);
-		rumorText = rumorText.replace(/\[([^[\]]+)\]/gmi, (correspondance, p1, decalage, chaine) => {
-			// Help: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/replace#Indiquer_une_fonction_comme_param%C3%A8tre
+		// Replaces all "[ ... / ... ]" by arrays,
+		// then chooses one of the values at random.
+		// Help: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_function_as_a_parameter
+		rumorText = rumorText.replace(/\[([^[\]]+)\]/gmi, (match, p1) => {
 			let text = '';
 			const options = p1.split('/');
 			if (options.length) {
 				const rnd = rand(1, options.length) - 1;
-				text += '**' + options[rnd].trim() + '**';
+				text += `**${options[rnd].trim()}**`;
 			}
 			return text;
 		});
 
-		const embed = new YZEmbed(Rumors.init + '...', rumorText);
+		const embed = new YZEmbed(`${Rumors.title} ...`, rumorText);
 
 		message.channel.send(embed);
 	},
 };
-
-function strLcFirst(str) {
-	return str.charAt(0).toLowerCase() + str.slice(1);
-}
