294587f39f6c0883b8c86e8cf48d6407c549ec72
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-02-28 17:02:53 +0100

    Fixed arg "all"

--- commands/artifact.js
+++ commands/artifact.js
@@ -4,17 +4,17 @@ const { rand } = require('../utils/utils.js');
 
 module.exports = {
 	name: 'artifact',
-	description: 'Draws a random artifact. Available sources are:'
+	description: 'Draws a random artifact from the MYZ core rulebook. Additional available sources are:'
 		+ '\n• `gla` – Mutant: GenLab Alpha'
 		+ '\n• `mech` – Mutant: Mechatron'
 		+ '\n• `ely` – Mutant: Elysium'
 		+ '\n• `astra` – Mutant: Ad Astra'
 		+ '\nMetaplot items are removed by default. Use `meta` to add them to the roll.'
-		+ '\nUse `all` to pick from all book sources.',
+		+ '\nUse `all` to pick from all book sources (including metaplot items).',
 	aliases: ['arto'],
 	guildOnly: false,
 	args: false,
-	usage: '[gla] [mech] [ely] [astra] [meta]',
+	usage: '[all | meta gla mech ely astra]',
 	execute(args, message) {
 		// Lists all legal books
 		// Using a "Set" object instead of a simple Array,
@@ -22,10 +22,10 @@ module.exports = {
 		const legalBooks = new Set();
 		for (const book in Artos) legalBooks.add(book);
 
+		// If "all", adds all books.
+		if (args.includes('all')) args = args.concat([...legalBooks]);
 		// Default book should be MYZ.
 		if (!args.includes('myz')) args.push('myz');
-		// If "all", adds all books.
-		if (args.includes('all')) args.concat(legalBooks);
 
 		// Using a "Set" object instead of a simple Array,
 		// because it avoids duplicates.
@@ -42,7 +42,7 @@ module.exports = {
 			}
 		});
 		const nb = rand(1, artifacts.size) - 1;
-		const artifact = Array.from(artifacts)[nb];
+		const artifact = [...artifacts][nb];
 
 		const embed = new YZEmbed('Artifact', artifact);
 		message.channel.send(embed);
