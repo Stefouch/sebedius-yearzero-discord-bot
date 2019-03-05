d7de5e8ad6ac8994c703cd299d07b0466e66f434
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-05 17:21:23 +0100

    Added extra comments for the code

--- commands/threat.js
+++ commands/threat.js
@@ -9,6 +9,7 @@ module.exports = {
 	guildOnly: false,
 	args: false,
 	execute(args, message) {
+		// Rolls for the threat type.
 		const nb = rand(1, 6);
 		let type;
 
@@ -16,7 +17,10 @@ module.exports = {
 		else if (nb <= 5) type = 'monsters';
 		else type = 'phenomenons';
 
+		// Collects the list of the threats for the chosen type.
 		const threats = Threats.myz[type];
+
+		// Rolls for the threat from the chosen type.
 		const rnd = rollD66();
 		let threat;
 
