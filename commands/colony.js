84fdeb4efc680623199f4be498be85ca8f757be2
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-12-04 17:33:30 +0100

    Added !planet

--- commands/colony.js
+++ commands/colony.js
@@ -1,6 +1,5 @@
 const YZEmbed = require('../util/YZEmbed');
 const Planet = require('../util/ALIENWorldGenerator');
-const Util = require('../util/Util');
 
 module.exports = {
 	name: 'colony',
@@ -13,38 +12,41 @@ module.exports = {
 		const o = new Planet('rocky', true, 1);
 		const embed = new YZEmbed(o.title, o.description);
 
-		// COLONY SIZE & POPULATION
-		const colo = o.colony;
-		embed.addField(
-			'Population',
-			`:busts_in_silhouette: × ${colo.population}\n(${colo.size})`,
-			true,
-		);
+		if (!args[0]) {
 
-		// COLONY MISSIONS
-		const missions = colo.missions;
-		embed.addField(
-			`Mission${(missions.size > 1) ? 's' : ''}`,
-			[...missions].join('\n'),
-			true,
-		);
+			// COLONY SIZE & POPULATION
+			const colo = o.colony;
+			embed.addField(
+				'Population',
+				`:busts_in_silhouette: × ${colo.population}\n(${colo.size})`,
+				true,
+			);
 
-		// COLONY ALLEGIANCE
-		embed.addField('Allegiance', colo.allegiance, true);
+			// COLONY MISSIONS
+			const missions = colo.missions;
+			embed.addField(
+				`Mission${(missions.size > 1) ? 's' : ''}`,
+				[...missions].join('\n'),
+				true,
+			);
 
-		// COLONY ORBIT
-		embed.addField('Orbit', o.orbits.join('\n'), true);
+			// COLONY ALLEGIANCE
+			embed.addField('Allegiance', colo.allegiance, true);
 
-		// COLONY FACTIONS
-		const factions = colo.factions;
-		embed.addField(
-			`Faction${(factions.qty > 1) ? 's' : ''}`,
-			`${factions.strengths}:\n- ${factions.types.join('\n- ')}`,
-			false,
-		);
+			// COLONY ORBIT
+			embed.addField('Orbit', o.orbits.join('\n'), true);
 
-		// COLONY HOOK
-		embed.addField('Event', colo.hook, false);
+			// COLONY FACTIONS
+			const factions = colo.factions;
+			embed.addField(
+				`Faction${(factions.qty > 1) ? 's' : ''}`,
+				`${factions.strengths}:\n- ${factions.types.join('\n- ')}`,
+				false,
+			);
+
+			// COLONY HOOK
+			embed.addField('Event', colo.hook, false);
+		}
 
 		return message.channel.send(embed);
 	},
