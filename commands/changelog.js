06551b0811a2e35780328fe7e89f7b5259d826a5
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-06-22 23:23:17 +0200

    Create changelog.js

---
+++ commands/changelog.js
@@ -0,0 +1,19 @@
+const YZEmbed = require('../util/YZEmbed');
+
+module.exports = {
+	name: 'changelog',
+	type: 'Core',
+	description: 'Prints a link to the official changelog.',
+	guildOnly: false,
+	args: false,
+	usage: '',
+	async execute(args, message, client) {
+		const embed = new YZEmbed(
+			'📑 Sebedius Changelog',
+			'You can check out the latest patch notes at '
+			+ 'https://github.com/Stefouch/sebedius-myz-discord-bot/blob/master/CHANGELOG.md',
+			message,
+		);
+		return message.channel.send(embed);
+	},
+};
