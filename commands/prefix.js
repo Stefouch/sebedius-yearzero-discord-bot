d958e8d8aef44c51154015941ec481d240cf2927
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-07-13 10:52:27 +0200

    Changed subcommand set prefix

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,13 +3,21 @@ const YZEmbed = require('../utils/embeds');
 module.exports = {
 	name: 'prefix',
 	group: 'Core',
-	description: 'Gets the prefixes for this server.',
+	description: 'Gets the prefixes for this server. Sets a new one with the option `add`.',
 	guildOnly: false,
 	args: false,
-	usage: '[new prefix]',
+	usage: '[set <new prefix>]',
 	async execute(args, ctx) {
-		if (args.length) {
-			return await ctx.bot.commands.get('setconf').execute(['prefix', args[0]], ctx);
+		if (args.length > 0) {
+			if (args[0].toLowerCase() === 'set' && args.length === 2) {
+				return await ctx.bot.commands.get('setconf').execute(['prefix', args[1]], ctx);
+			}
+			else {
+				return await ctx.reply(
+					':information_source: Invalid arguments. '
+					+ `The proper usage would be \`${ctx.prefix}prefix ${module.exports.usage}\`.`,
+				);
+			}
 		}
 		const msg = `1. ${ctx.bot.mention}\n2. ${ctx.prefix}`;
 		const embed = new YZEmbed('Sebedius Prefixes', msg).setFooter('2 prefixes');
