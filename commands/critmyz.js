20ca276aeb5f3592e423f9bb359daaab7382cfec
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-07-14 16:19:04 +0200

    Command renamed

--- commands/critmyz.js
+++ commands/critmyz.js
@@ -1,9 +1,9 @@
 module.exports = {
-	name: 'critmutant',
+	name: 'critmyz',
 	group: 'Mutant: Year Zero',
 	description: 'Rolls for a random critical injury.'
 		+ '\nType `help crit` for more details.',
-	aliases: ['critm'],
+	aliases: ['critm', 'critmutant'],
 	guildOnly: false,
 	args: false,
 	usage: '[table] [numeric]',
