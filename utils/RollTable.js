aee2fbd3bb458046328554d95cc52aac1a5990b8
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-09-10 01:05:32 +0200

    refactor(): random()

--- utils/RollTable.js
+++ utils/RollTable.js
@@ -1,3 +1,5 @@
+const { rand, hasSameDigits, convertToBijective } = require('./Util');
+
 /**
  * A rollable table.
  * @extends {Map}
@@ -5,11 +7,79 @@
 class RollTable extends Map {
 	/**
 	 * @param {string} name The name of the table
-	 * @param {*} iterable An Array or other iterable object whose elements are key-value pairs.
+	 * @param {?string} roll The die (or dice) required to roll the table
+	 * @param {?*} iterable An Array or another iterable object whose elements are key-value pairs
 	 */
-	constructor(name, iterable) {
+	constructor(name, roll = null, iterable) {
 		super(iterable);
+
+		/**
+		 * The name of the table.
+		 * @type {string}
+		 */
 		this.name = name;
+
+		/**
+		 * The die (or dice) required to roll the table.
+		 * @type {string}
+		 */
+		this.roll = /^d\d+$/i.test(roll) ? roll.toUpperCase() : null;
+	}
+
+	/**
+	 * The lowest reference.
+	 * @type {number}
+	 * @readonly
+	 */
+	get min() {
+		let n = Number.MAX_VALUE;
+
+		for (const key of this.keys()) {
+			if (typeof key === 'string') {
+				if (/-/.test(key)) {
+					const boundaries = key.split('-');
+					const min = +boundaries[0];
+					// const max = +boundaries[1];
+					n = Math.min(n, min);
+				}
+				else if (/^[-+]?\d+$/.test(key)) {
+					n = Math.min(n, +key);
+				}
+			}
+			else if (typeof key === 'number') {
+				n = Math.min(n, key);
+			}
+		}
+		if (n === Number.MAX_VALUE) return undefined;
+		return n;
+	}
+
+	/**
+	 * The highest reference.
+	 * @type {number}
+	 * @readonly
+	 */
+	get max() {
+		let n = 0;
+
+		for (const key of this.keys()) {
+			if (typeof key === 'string') {
+				if (/-/.test(key)) {
+					const boundaries = key.split('-');
+					// const min = +boundaries[0];
+					const max = +boundaries[1];
+					n = Math.max(n, max);
+				}
+				else if (/^[-+]?\d+$/.test(key)) {
+					n = Math.max(n, +key);
+				}
+			}
+			else if (typeof key === 'number') {
+				n = Math.max(n, key);
+			}
+		}
+		if (n <= 0) return undefined;
+		return n;
 	}
 
 	/**
@@ -19,21 +89,29 @@ class RollTable extends Map {
 	 * @readonly
 	 */
 	get length() {
-		let len = 0;
-
-		for (const key of this.keys()) {
-			if (typeof key === 'string') {
-				if (/-/.test(key)) {
-					const boundaries = key.split('-');
-					const min = +boundaries[0];
-					const max = +boundaries[1];
-					len += (max - min + 1);
-				}
-				else { len++; }
+		if (this.min <= 1) return this.max;
+		if (this.min === 11 || this.min === 111) {
+			if (hasSameDigits(this.max)) {
+				return (+`${this.max}`.charAt(0)) ** Math.ceil(Math.log10(this.max));
 			}
-			else { len++; }
 		}
-		return len;
+		return Math.round(this.max / this.min);
+
+		//let len = 0;
+
+		// for (const key of this.keys()) {
+		// 	if (typeof key === 'string') {
+		// 		if (/-/.test(key)) {
+		// 			const boundaries = key.split('-');
+		// 			const min = +boundaries[0];
+		// 			const max = +boundaries[1];
+		// 			len += (max - min + 1);
+		// 		}
+		// 		else { len++; }
+		// 	}
+		// 	else { len++; }
+		// }
+		// return len;
 	}
 
 	/**
@@ -42,10 +120,11 @@ class RollTable extends Map {
 	 * @readonly
 	 */
 	get d() {
+		if (this.roll) return this.roll;
 		if (this.length === 6) return 'D6';
-		else if (this.length === 36) return 'D66';
-		else if (this.length === 216) return 'D666';
-		else return `D${this.length}`;
+		if (this.length === 36) return 'D66';
+		if (this.length === 216) return 'D666';
+		return `D${this.length}`;
 	}
 
 	/**
@@ -95,6 +174,42 @@ class RollTable extends Map {
 			}
 		}
 		return super.has(reference);
+	}
+
+	/**
+	 * Returns a random entry from a RollTable object.
+	 * @returns {*} or `undefined` if not found
+	 */
+	random() {
+		// Generates a random seed based on the quantity of entries (length).
+		let seed = rand(1, this.length);
+
+		// Praises the RNG Jesus.
+		let rngesus = seed;
+
+		// If the roll uses a D66 or D66 (or something similar)
+		// `rngesus` must be adapted.
+		if (this.min === 11 || this.min === 111) {
+			if (hasSameDigits(this.max)) {
+				// Gets the actual Base. D66 = 6; D88 = 8;
+				const digit = +`${this.max}`.charAt(0);
+
+				// Adjusts the seed.
+				let log = Math.floor(Math.log10(this.max));
+				for (; log > 0; log--) seed += digit ** log;
+
+				// Creates sequence for the Base <digit>
+				const seq = Array(digit)
+					.fill()
+					.map((x, i) => i + 1)
+					.join('');
+
+				// Converts the seed into a valid reference.
+				rngesus = convertToBijective(seed, seq);
+			}
+		}
+		// Returns the value of the random reference.
+		return this.get(rngesus);
 	}
 }
 
