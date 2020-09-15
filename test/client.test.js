d035008b245570cb496ac4c6505fe463d2d9cc50
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-09-15 16:44:13 +0200

    feat(tests): Validates each commands

--- test/client.test.js
+++ test/client.test.js
@@ -2,26 +2,38 @@
 
 const { describe, it } = require('mocha');
 const expect = require('chai').expect;
-const should = require('chai').should();
+// const should = require('chai').should();
 const Discord = require('discord.js');
 const Sebedius = require('../Sebedius');
+const ContextMessage = require('../utils/ContextMessage');
 
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
 
-const ctxModel = new Discord.Message();
-
 describe('Discord Bot Client', function() {
 	// this.slow(500);
 	this.timeout(5000);
 	// this.retries(3);
-	const token = process.env.DISCORD_BETA_BOT_TOKEN || process.env.TOKEN;
+	const token = process.env.TOKEN;
+	const bot = new Sebedius(require('../config.json'));
 
-	it('Should log in without errors', async function() {
-		// const bot = new Discord.Client();
-		// await expect(() => bot.login(token)).to.not.throw();
-		expect(1).to.eq(1);
-		// bot.destroy();
+	it('Sebedius is created without error', function() {
+		expect(bot.config.defaultPrefix === '!');
+	});
+
+	describe('# Test each commad', function() {
+		for (const [cmdName, cmd] of bot.commands) {
+
+			it(`Command: ${cmdName}`, function() {
+				expect(cmd.name).to.equal(cmdName);
+				expect(cmd.category).to.be.a('string').with.length.greaterThan(1);
+				expect(cmd.description).to.be.a('string').with.length.greaterThan(1);
+
+				expect(
+					cmd.run(['Hi!'], new ContextMessage('!', bot, {}, new Discord.TextChannel())),
+				).to.be.a('Message');
+			});
+		}
 	});
 });
