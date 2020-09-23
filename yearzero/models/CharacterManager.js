cb2bbe10cf4018880270af8b6f8c9bcf173b102f
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-09-24 00:19:15 +0200

    Update CharacterManager.js

--- yearzero/models/CharacterManager.js
+++ yearzero/models/CharacterManager.js
@@ -30,16 +30,16 @@ class CharacterManager {
 
 	/**
 	 * Adds to cache.
-	 * @param {*} data Character to add
+	 * @param {Object|Character} data Character to add
 	 * @param {boolean} cache Whether to add the character to the cache
-	 * @param {Object} options
+	 * @param {Object} options Additional options
+	 * @param {?string} options.id Custom owner's ID
 	 * @returns {Character}
 	 */
 	add(data, cache = true, { id } = {}) {
 		// const existing = this.cache.get(id || data.id);
 		// if (existing) return existing;
-
-		const character = data instanceof Character ? data : this.create(data);
+		const character = data instanceof Character ? data : this.create(data, id);
 		if (cache) this.cache.set(id || character.id, character);
 		return character;
 	}
@@ -47,106 +47,115 @@ class CharacterManager {
 	/**
 	 * Creates a Character.
 	 * @param {Object} data Raw data of the character
+	 * @param {?string} owner Custom owner's ID
 	 * @returns {ForbiddenLandsCharacter|Character}
 	 */
-	create(data) {
-		if (data.game === 'fbl') return new ForbiddenLandsCharacter(data.owner, data);
+	create(data, owner) {
+		if (data.game === 'fbl') return new ForbiddenLandsCharacter(owner || data.owner, data);
 		return new Character(data.owner, data);
 	}
 
 	/**
-	 * Removes some entries from the cache.
-	 * @param {import('discord.js').Snowflake} id Character- and/or owner's ID
-	 */
-	remove(id) {
-		this.cache.sweep(c => c.id === id || c.owner === id);
-	}
-
-	/**
-	 * Removes all entries from the cache and the database for a single player.
-	 * @param {import('discord.js').Snowflake} id Owner's ID
+	 * Fetches a character from the cache and/or the database.
+	 * @param {string} ownerID Owner's ID
+	 * @param {boolean} cache Whether to add the fetched character to the cache
+	 * @param {string} force Whether to skip the cache check
+	 * @returns {Character}
 	 * @async
 	 */
-	async cleanse(id) {
-		this.remove(id);
-		await this.store.delete(id);
-	}
-
-	/**
-	 * Saves a character to the database.
-	 * @param {Character} character Character to save.
-	 * @param {boolean} cleanse Whether old character entries should be removed in the process.
-	 * @returns {Character} The saved character.
-	 * @async
-	 */
-	async commit(character, cleanse = true) {
-		// Gets all the existing player's characters.
-		/** @type {Character[]} but in raw */
-		let characters = await this.store.get(character.owner) || [];
-
-		// Removes older entries.
-		if (cleanse) {
-			const now = Date.now();
-			characters = characters.filter(c => now - c.ttl <= this.constructor.TTL);
-		}
-
-		// Removes existing entry(ies) that will be replaced.
-		characters = characters.filter(c => c.id !== character.id);
-
-		// Adds the new character to the list.
-		characters.push(character.toRaw());
-
-		// Stores in the database.
-		const isSet = await this.store.set(character.owner, characters, this.constructor.TTL);
-		if (!isSet) throw new Error(`Could not save the character(s) to the database for owner: ${character.owner}`);
-		return this.add(character);
-	}
-
-	/**
-	 * Obtains a character from the database, or the character cache if it's already available.
-	 * @param {import('discord.js').Snowflake} characterID Character's ID
-	 * @param {import('discord.js').Snowflake} ownerID Owner's ID
-	 * @param {boolean} [cache=true] Whether to cache the new character object if it isn't already
-	 * @param {boolean} [force=false] Whether to skip the cache check and request the API
-	 * @returns {Promise<Character>}
-	 */
-	async fetch(characterID, ownerID, cache = true, force = false) {
+	async fetch(ownerID, cache = true, force = false) {
 		if (!force) {
-			const existing = this.cache.get(characterID);
+			const existing = this.cache.find(c => c.owner === ownerID && c.active);
 			if (existing) return existing;
 		}
-		const datas = await this.fetchAll(ownerID, cache);
-		const data = datas.find(c => c.id === characterID);
-		return this.add(data, cache);
+
+		const characters = await this.store.get(ownerID);
+		const activeCharacter = characters.find(c => c.active);
+		return this.add(activeCharacter, cache);
 	}
 
 	/**
-	 * Obtains all characters of a single player from the database.
-	 * @param {import('discord.js').Snowflake} ownerID Owner's ID
-	 * @param {boolean} [cache=false] Whether to cache the new character object if it isn't already
-	 * @returns {Promise<Character[]>}
+	 * Deletes a Character from the cache and the database.
+	 * If `charID` is omitted, instead deletes all the owner's Characters.
+	 * @param {string} ownerID Owner's ID
+	 * @param {string} charID Character's ID
+	 * @async
 	 */
-	async fetchAll(ownerID, cache = false) {
-		const datas = await this.store.get(ownerID) || [];
-		if (cache) for (const data of datas) this.add(data);
-		return datas;
+	async delete(ownerID, charID) {
+		if (!charID) {
+			this.cache.sweep(c => c.owner === ownerID);
+			await this.store.delete(ownerID);
+		}
+		else {
+			this.cache.delete(charID);
+			const characters = (await this.store.get(ownerID) || [])
+				.filter(c => c.id !== charID);
+
+			if (!characters.length) await this.store.delete(ownerID);
+			else await this.commit(ownerID, characters);
+		}
+	}
+
+	/**
+	 * Saves a Character in the database.
+	 * @param {Character} character Character to save in the database
+	 * @async
+	 */
+	async save(character) {
+		const characters = (await this.store.get(character.owner) || [])
+			.filter(c => c.id !== character.id);
+
+		characters.push(character instanceof Character ? character.toRaw() : character);
+		await this.commit(character.owner, characters);
+	}
+
+	/**
+	 * Sets a Character as the active one.
+	 * @param {Character} character Character to set active
+	 * @async
+	 */
+	async setActive(character) {
+		const characters = (await this.store.get(character.owner) || []);
+		characters.forEach(c => c.active = false);
+
+		const activeCharacter = characters.find(c => c.id === character.id);
+		character.active = true;
+		activeCharacter.active = true;
+
+		await this.commit(character.owner, characters);
+	}
+
+	/**
+	 * Saves all the player's characters to the database.
+	 * @param {string} ownerID Owner's ID
+	 * @param {Character[]} characters Array of raw Characters
+	 * @async
+	 */
+	async commit(ownerID, characters) {
+		const isSet = await this.store.set(ownerID, characters);
+		if (!isSet) throw new Error('@CharacterManager #Commit - COULD NOT SAVE TO DATABASE');
 	}
 
 	/**
 	 * Imports a character from an API.
 	 * @param {import('discord.js').Snowflake} ownerID Owner's ID
 	 * @param {string} url URL where to fetch the character's data
+	 * @param {boolean} [active=true] Whether to set the imported character as active
+	 * @param {boolean} [cache=true] Whether to store the character in the cache
+	 * @param {boolean} [store=true] Whether to store the character in the database
 	 * @returns {Promise<Character>}
 	 * @async
 	 */
-	async import(ownerID, url) {
+	async import(ownerID, url, active = true, cache = true, store = true) {
 		let character;
 		if (LasseForbiddenSheetParser.URL_REGEX.test(url)) {
 			character = await new LasseForbiddenSheetParser(url)
 				.loadCharacter(ownerID);
 		}
-		if (!character) throw new ReferenceError('@CharacterManager Import API - Unknown URL');
-		return this.add(character);
+		if (!character) throw new ReferenceError('@CharacterManager #Import - Unknown URL');
+		if (store) await this.save(character);
+		if (active) await this.setActive(character);
+		return this.add(character, cache);
 	}
 }
 
