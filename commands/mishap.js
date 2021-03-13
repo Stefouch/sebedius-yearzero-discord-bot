989955c37ba338f7dae7902efe381aa88c140fb6
Michael Prangenberg <prangenberg@edv-ermtraud.de>
2021-03-13 11:02:12 +0100

    Translated mishap command

--- commands/mishap.js
+++ commands/mishap.js
@@ -1,24 +1,38 @@
 const { getTable } = require('../Sebedius');
 const { rollD66 } = require('../utils/Util');
 const { YZEmbed } = require('../utils/embeds');
+const { __ } = require('../lang/locales');
 
 module.exports = {
 	name: 'mishap',
 	category: 'fbl',
-	description: 'Draws a random Magic Mishap.',
+	description: 'cmishap-description',
 	guildOnly: false,
 	args: false,
 	usage: '[reference]',
 	async run(args, ctx) {
-		if (args[0] && !/[123456]{2}/.test(args[0])) {
-			return await ctx.reply('⚠️ Invalid Magic Mishap\'s reference!');
+		// Parses arguments.
+		const argv = require('yargs-parser')(args, {
+			string: ['lang'],
+			alias: {
+				lang: ['lng', 'language'],
+			},
+			default: {
+				lang: null,
+			},
+			configuration: ctx.bot.config.yargs,
+		});
+		const lang = await ctx.bot.getValidLanguageCode(argv.lang, ctx);
+
+		if (argv._[0] && !/[123456]{2}/.test(argv._[0])) {
+			return await ctx.reply(`⚠️ ${__('cmishap-invalid-reference', lang)}!`);
 		}
-		const mishapTable = getTable('MAGIC_MISHAP', './gamedata/fbl/', 'fbl-magic-mishaps', 'en', 'csv');
-		const ref = +args[0] || rollD66();
+		const mishapTable = getTable('MAGIC_MISHAP', './gamedata/fbl/', 'fbl-magic-mishaps', lang, 'csv');
+		const ref = +argv._[0] || rollD66();
 		const mishap = mishapTable.get(ref);
 
 		const embed = new YZEmbed(
-			`Magic Mishap (${ref})`,
+			`${__('magic-mishap', lang)} (${ref})`,
 			mishap.effect.replace('{prefix}', ctx.prefix),
 			ctx,
 			true,
