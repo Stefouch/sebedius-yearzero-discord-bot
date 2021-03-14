7d4f7ee8db3a8e3f8ac8a4bdc00b616efafc5f01
mprangenberg <github@isteineprinzessin.de>
2021-03-14 14:41:21 +0100

    Translated colony descriptions

--- commands/planet.js
+++ commands/planet.js
@@ -2,10 +2,11 @@ module.exports = {
 	name: 'planet',
 	aliases: ['world'],
 	category: 'alien',
-	description: 'Generates an uncolonized planet for the Alien RPG.',
+	description: 'cplanet-description',
+	moreDescriptions: 'cplanet-moredescriptions',
 	guildOnly: false,
 	args: false,
-	usage: '[-type planet-type] [-lang language_code]',
+	usage: '[name] [-type planet-type] [-lang language_code]',
 	async run(args, ctx) {
 		// Exits early and executes "!colony -uncolonized".
 		args.push('-uncolonized');
