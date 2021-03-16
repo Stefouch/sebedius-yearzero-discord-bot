7890638ee268d27496b15dac2a4e922c22bbde81
Stefouch <47865544+Stefouch@users.noreply.github.com>
2021-03-16 13:42:26 +0100

    fix(lang): correct use of empty strings for language keys
    fixes #98 closes #98

--- lang/locales.js
+++ lang/locales.js
@@ -13,12 +13,13 @@ const LOCALES = {
  * Returns a locale entry (translated text)
  * @param {string} text The key to look for in the translation table
  * @param {string} locale The language code to use. Default is en (english)
+ * @returns {string} The translated text
  */
 module.exports.__ = (text, locale) => {
 	if (typeof text !== 'string') return text;
 	text = text.replace(/_/g, '-').toLowerCase();
-	const loc = LOCALES[locale] ? locale : 'en';
-	if (LOCALES[loc][text]) return LOCALES[loc][text];
-	else if (LOCALES.en[text]) return LOCALES.en[text];
+	const loc = locale in LOCALES ? locale : 'en';
+	if (LOCALES[loc][text] != undefined) return LOCALES[loc][text];
+	if (LOCALES.en[text] != undefined) return LOCALES.en[text];
 	return text;
 };
