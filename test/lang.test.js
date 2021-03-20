34c23aecec43a78d66c33cc01d297d61f3373482
Stefouch <47865544+Stefouch@users.noreply.github.com>
2021-03-20 15:21:52 +0100

    lint: use of origin linter

--- test/lang.test.js
+++ test/lang.test.js
@@ -9,7 +9,7 @@ const LANGUAGE_CODES = Object.keys(SUPPORTED_LANGS);
 
 const size = Object.keys(en).length;
 
-describe('Localisation', function () {
+describe('Localisation', function() {
 
 	for (const lang of LANGUAGE_CODES) {
 		// Skips English as it's the default language.
@@ -25,39 +25,39 @@ describe('Localisation', function () {
 			locale = {};
 		}
 
-		describe(`# Language: ${lang.toUpperCase()}`, function () {
+		describe(`# Language: ${lang.toUpperCase()}`, function() {
 
-			it(`Should have a language file "${lang}.json"`, function () {
+			it(`Should have a language file "${lang}.json"`, function() {
 				expect(locale, `lang/${lang}.json`).to.be.an('object').and.not.empty;
 			});
 
-			it(`Should have ${size} translation keys`, function () {
+			it(`Should have ${size} translation keys`, function() {
 				expect(Object.keys(locale)).to.have.lengthOf(size);
 			});
 
-			it('Should have all en.json keys', function () {
+			it('Should have all en.json keys', function() {
 				const out = [];
 				for (const key in en) if (!(key in locale)) out.push(key);
 				expect(out.join(), `Missing keys from ${lang}.json`).to.equal([].join());
 				expect(out).to.have.lengthOf(0);
 			});
 
-			it('Should not have any unlisted keys', function () {
+			it('Should not have any unlisted keys', function() {
 				const out = [];
 				for (const key in locale) if (!(key in en)) out.push(key);
 				expect(out.join(), 'Missing keys from en.json').to.equal([].join());
 				expect(out).to.have.lengthOf(0);
 			});
 
-			it('Should not have any issue to translate', function () {
+			it('Should not have any issue to translate', function() {
 				for (const key in locale) {
-					expect(locale[key]).to.satisfy(function (str) {
+					expect(locale[key]).to.satisfy(function(str) {
 						return str === __(key, lang);
 					});
 				}
 			});
 
-			it('Should have array keys with equal or greater length', function () {
+			it('Should have array keys with equal or greater length', function() {
 				const out = [];
 				for (const [key, translation] of Object.entries(locale)) {
 					if (!Array.isArray(translation)) continue;
@@ -67,21 +67,23 @@ describe('Localisation', function () {
 				expect(out).to.have.lengthOf(0);
 			});
 
-			it('Every call for translation should return a translated value instead of the key name', function () {
+			it('Every call for translation should return a translated value instead of the key name', function() {
 				const allJsFiles = glob.sync('{*.js,!(node_modules)/**/*.js}');
-				for (const commandfile of allJsFiles) {
-					fs.readFile(commandfile, 'utf8', (error, data) => {
-						expect(error, `Loading ${commandfile} failed`).to.be.null;
+				for (const jsFile of allJsFiles) {
+					fs.readFile(jsFile, 'utf8', (error, data) => {
+						expect(error, `Loading ${jsFile} failed`).to.be.null;
 						if (!error) {
 							const regex = /__\(['"](.*?)['"],.*?\)/gm;
 							let result;
 							while ((result = regex.exec(data)) !== null) {
-								expect(__(result[1], lang), `Translation call that returns it's key. (${result[0]} in ${commandfile})`).to.not.be.equal(result[1]);
+								expect(
+									__(result[1], lang),
+									`Translation call that returns it's key. (${result[0]} in ${jsFile})`,
+								).to.not.be.equal(result[1]);
 							}
 						}
-					})
+					});
 				}
-
 			});
 
 		});
