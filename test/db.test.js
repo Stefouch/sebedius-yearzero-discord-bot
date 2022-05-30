c1f9422ccc0d630069adb35256248427308e167d
Stefouch <stefouch@gmail.com>
2022-05-30 12:47:32 +0200

    test: ssl bug

--- test/db.test.js
+++ test/db.test.js
@@ -2,6 +2,7 @@ const { describe, it } = require('mocha');
 const expect = require('chai').expect;
 const Keyv = require('keyv');
 const Util = require('../utils/Util');
+const dbParams = '?ssl=true&sslmode=require&sslfactory=org.postgresql.ssl.NonValidatingFactory';
 
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
@@ -38,7 +39,7 @@ describe('Keyv & PostGreSQL Database', function() {
 	});
 
 	it(`Should be able to create a Keyv DB (${name})`, async function() {
-		kdb = new Keyv(process.env.DATABASE_URL + '?ssl=true', { namespace: name });
+		kdb = new Keyv(process.env.DATABASE_URL + dbParams, { namespace: name });
 		kdb.on('error', err => console.error(`Keyv Connection Error: ${name.toUpperCase()}S\n`, err));
 		expect(kdb.opts.store.namespace, `The DB's namespace should be "${name}"`).to.equal(name);
 
