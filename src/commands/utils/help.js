9d2f0434f6a4a61d3c8a172c9ff464983e76e988
Stefouch <stefouch@gmail.com>
2022-08-29 17:31:04 +0200

    feat(Help): ✨ Add more fields

--- src/commands/utils/help.js
+++ src/commands/utils/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, hyperlink, inlineCode } = require('discord.js');
 const SebediusCommand = require('../../structures/command');
 
 module.exports = class HelpCommand extends SebediusCommand {
@@ -19,29 +19,56 @@ module.exports = class HelpCommand extends SebediusCommand {
   /** @type {SebediusCommand.SebediusCommandRunFunction} */
   async run(interaction, t) {
     const embed = new EmbedBuilder();
-    const bot = interaction.client;
     const commandName = interaction.options.getString('command');
 
     if (!commandName) {
       embed
         .setTitle('**Sebedius – Year Zero Discord Bot**')
+        .setDescription(t('commands:help.fullDescription') + ' https://github.com/Stefouch/sebedius-myz-discord-bot')
+        .setTimestamp()
         .addFields({
-          name: `🏁 ${t('command:help.deployed_version')}`,
-          value: bot.version,
+          name: `🏁 ${t('commands:help.deployedVersion')}`,
+          value: this.bot.version,
           inline: true,
         }, {
-          name: `🛠 ${t('command:help.developer')}`,
-          value: (await bot.getUser(bot.ownerId)).toString(),
+          name: `🛠 ${t('commands:help.developer')}`,
+          value: `${(await this.bot.getUser(this.bot.ownerId)).toString()}\n${hyperlink('stefouch.be', 'https://www.stefouch.be')}`,
           inline: true,
         }, {
           name: '🐦 Twitter',
-          value: 'https://twitter.com/stefouch',
+          value: hyperlink('@stefouch', 'https://twitter.com/stefouch'),
           inline: true,
         }, {
-          name: `📖 ${t('command:help.readme')}`,
-          value: 'https://github.com/Stefouch/sebedius-yearzero-discord-bot/blob/master/README.md',
+          name: `🔗 ${t('commands:help.usefulLinks')}`,
+          value: `• ${hyperlink(t('commands:help.addToServer'), this.bot.inviteURL)}`
+            + `\n• ${hyperlink(t('commands:help.readTheWiki'), this.bot.config.wikiURL)}`
+            + `\n• ${hyperlink(t('commands:help.githubIssues'), this.bot.config.issueURL)}`,
+          inline: false,
+        }, {
+          name: '❤ Patreon',
+          value: t('commands:help.supportOnPatreon', {
+            url: hyperlink(t('commands:help.patreonPage'), 'https://patreon.com/Stefouch'),
+          }),
+          inline: false,
+        }, {
+          name: `🗒 ${t('commands:help.listOfCommands')}`,
+          value: t('commands:help.listOfCommandsFullDescription', {
+            helpAll: inlineCode('/help all'),
+            helpCommand: inlineCode('/help <command>'),
+          }),
           inline: false,
         });
     }
+    // Help message with the list of all commands.
+    else if (commandName === 'all') {}
+    // Help message for a specific command.
+    else {
+      const command = this.bot.commands.get(commandName);
+
+      if (!command) {
+        return interaction.reply('🤷 Command not found!');
+      }
+    }
+    await interaction.reply({ embeds: [embed] });
   }
 };
