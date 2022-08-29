0dcc48c03447f95ab6137eb589c1c1c89c23eefa
Stefouch <stefouch@gmail.com>
2022-08-29 17:28:04 +0200

    refactor(i18n): ♻️ 🌐 Use of namespaces

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -2,22 +2,27 @@ const i18next = require('i18next');
 const i18nextBackend = require('i18next-fs-backend');
 const { stringify, parse } = require('yaml');
 
-i18next
-  // @ts-ignore
-  .use(i18nextBackend)
-  .init({
-    fallbackLng: 'en',
-    supportedLngs: ['en'],
-    // debug: true,
-    // initImmediate: false,
-    backend: {
-      loadPath: './src/lang/{{lng}}.yml',
-      addPath: './src/lang/{{lng}}.missing.yml',
-      stringify: data => stringify(data),
-      parse: data => parse(data),
-    },
-  })
-  .then(() => console.log('✔ Sebedius is translated!'))
-  .catch(err => console.error(err));
-
-module.exports = i18next;
+module.exports = async () => {
+  await i18next
+    // @ts-ignore
+    .use(i18nextBackend)
+    .init({
+      supportedLngs: ['en'],
+      fallbackLng: 'en',
+      ns: ['commands'],
+      defaultNS: 'commands',
+      interpolation: {
+        escapeValue: false,
+      },
+      // debug: true,
+      // initImmediate: false,
+      backend: {
+        loadPath: './src/locales/{{lng}}/{{ns}}.yml',
+        addPath: './src/locales/{{lng}}/{{ns}}.missing.yml',
+        stringify: data => stringify(data),
+        parse: data => parse(data),
+      },
+    })
+    .then(() => console.log('✔ Sebedius is translated!'))
+    .catch(err => console.error(err));
+};
