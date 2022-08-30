43720dddd06dea3f2799fe3faf474c87a286bd2a
Stefouch <stefouch@gmail.com>
2022-08-30 15:23:38 +0200

    refactor(Constants): ♻️ Change YearZeroGames codes
    for better human readability

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,30 @@
 /** @enum {string} */
 const YearZeroGames = {
-  BLANK: '0',
+  BLANK: 'generic',
   ALIEN_RPG: 'alien',
-  BLADE_RUNNER: 'br',
-  CORIOLIS: 'cor',
+  BLADE_RUNNER: 'bladerunner',
+  CORIOLIS: 'coriolis',
   FORBIDDEN_LANDS: 'fbl',
   MUTANT_YEAR_ZERO: 'myz',
   TALES_FROM_THE_LOOP: 'tftl',
   TWILIGHT_2K: 't2k',
-  VAESEN: 'vae',
+  VAESEN: 'vaesen',
+};
+
+/** @enum {string} */
+const YearZeroGameNames = {
+  [YearZeroGames.BLANK]: 'Generic',
+  [YearZeroGames.ALIEN_RPG]: 'Alien RPG',
+  [YearZeroGames.BLADE_RUNNER]: 'Blade Runner RPG',
+  [YearZeroGames.CORIOLIS]: 'Coriolis: The Third Horizon',
+  [YearZeroGames.FORBIDDEN_LANDS]: 'Forbidden Lands',
+  [YearZeroGames.MUTANT_YEAR_ZERO]: 'Mutant: Year Zero',
+  [YearZeroGames.TALES_FROM_THE_LOOP]: 'Tales From the Loop',
+  [YearZeroGames.TWILIGHT_2K]: 'Twilight 2000 4E',
+  [YearZeroGames.VAESEN]: 'Vaesen',
 };
 
 module.exports = {
   YearZeroGames,
+  YearZeroGameNames,
 };
