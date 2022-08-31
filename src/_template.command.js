2e557029792a071e41cb17d67bbd01b597659db2
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-08-31 16:39:16 +0200

    feat(Commands): ✨ Add command template

---
+++ src/_template.command.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const SebediusCommand = require('./structures/command');
+
+module.exports = class RollD66Command extends SebediusCommand {
+  constructor(client) {
+    super(client, {
+      ownerOnly: true, // <- Command is hidden & available for the bot owner only
+      category: SebediusCommand.CategoryFlagsBits.ROLL,
+      data: new SlashCommandBuilder()
+        .setName('cmd')
+        .setDescription('Template command'),
+      // etc...
+    });
+  }
+  /** @type {SebediusCommand.SebediusCommandRunFunction} */
+  async run(interaction, t, game) {
+    // Add the command effects here.
+  }
+};
