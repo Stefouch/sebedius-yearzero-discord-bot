e78c73986d17d4df5894f24a76c76af09c9cbd60
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-08-31 16:38:54 +0200

    feat(Commands): ✨ Add /rolld66

---
+++ src/commands/roll/rolld66.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder, inlineCode, EmbedBuilder } = require('discord.js');
+const SebediusCommand = require('../../structures/command');
+const YearZeroRoll = require('../../yearzero/roller/yzroll');
+const { YearZeroGameChoices, YearZeroGameNames } = require('../../constants');
+
+module.exports = class RollD66Command extends SebediusCommand {
+  constructor(client) {
+    super(client, {
+      category: SebediusCommand.CategoryFlagsBits.ROLL,
+      data: new SlashCommandBuilder()
+        .setName('rolld66')
+        .setDescription('Roll a d6, d66 or d666 die')
+        .addStringOption(opt => opt
+          .setName('die')
+          .setDescription('Choose d6, d66 or d666')
+          .addChoices(
+            { name: 'D6', value: 'D6' },
+            { name: 'D66', value: 'D66' },
+            { name: 'D666', value: 'D666' })
+          .setRequired(true))
+        .addStringOption(opt => opt
+          .setName('title')
+          .setDescription('Define a title for the roll'))
+        .addBooleanOption(opt => opt
+          .setName('private')
+          .setDescription('Whether to hide the roll from other players'))
+        .addStringOption(opt => opt
+          .setName('game')
+          .setDescription('Override the default chosen game which is used to render the rolled dice')
+          .addChoices(...YearZeroGameChoices)),
+    });
+  }
+  /** @type {SebediusCommand.SebediusCommandRunFunction} */
+  async run(interaction, t) {
+    const game = interaction.options.getString('game') || await this.bot.getGame(interaction.guildId);
+    const die = interaction.options.getString('die');
+
+    const n = die.split('6').length - 1;
+
+    const roll = new YearZeroRoll({
+      game,
+      name: interaction.options.getString('title') || inlineCode(die),
+      author: interaction.member,
+    }).addBaseDice(n);
+
+    await roll.roll();
+
+    const embed = new EmbedBuilder()
+      .setTitle(roll.name)
+      .setColor(this.bot.config.favoriteColor)
+      .setTimestamp()
+      .setFooter({ text: YearZeroGameNames[roll.game] })
+      .setDescription(t('commands:roll.resultForD66', {
+        author: roll.author.toString(),
+        dice: inlineCode(die),
+        result: `__**${roll.sumProductBaseTen()}**__`,
+      }));
+
+    return interaction.reply({
+      content: roll.emojify(),
+      embeds: [embed],
+      ephemeral: interaction.options.getBoolean('private'),
+    });
+  }
+};
