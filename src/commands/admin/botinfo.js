bbf08384161311c0424b79570707d93bfcb8fe92
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-09-01 23:57:42 +0200

    feat(Commands): ✨ Add /botinfo

---
+++ src/commands/admin/botinfo.js
@@ -0,0 +1,88 @@
+const os = require('node:os');
+const ms = require('ms');
+const worker = require('core-worker');
+const { SlashCommandBuilder, EmbedBuilder, version } = require('discord.js');
+const SebediusCommand = require('../../structures/command');
+
+module.exports = class BotInfoCommand extends SebediusCommand {
+  constructor(client) {
+    super(client, {
+      ownerOnly: true,
+      category: SebediusCommand.CategoryFlagsBits.ADMIN,
+      data: new SlashCommandBuilder()
+        .setName('botinfo')
+        .setDescription('Display information about the bot process'),
+    });
+  }
+  /** @type {SebediusCommand.SebediusCommandRunFunction} */
+  async run(interaction, t) {
+    if (interaction.user.id !== this.bot.ownerId) {
+      return interaction.reply({
+        content: `⛔ ${t('commands:ownerOnlyCommandDisclaimer')}`,
+        ephemeral: true,
+      });
+    }
+
+    await interaction.deferReply({ ephemeral:true });
+
+    const npmv = await worker.process('npm -v').death();
+    const embed = new EmbedBuilder()
+      .setTitle('`Sebedius Statistics`')
+      .setTimestamp()
+      .addFields({
+        name: 'Memory Usage',
+        value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+        inline: true,
+      }, {
+        name: 'Swap Partition Size',
+        value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`,
+        inline: true,
+      }, {
+        name: 'Uptime',
+        value: ms(this.bot.uptime),
+        inline: true,
+      }, {
+        name: 'Users',
+        value: '' + await this.bot.getUsersCount(),
+        inline: true,
+      }, {
+        name: 'Servers',
+        value: '' + await this.bot.getGuildsCount(),
+        inline: true,
+      }, {
+        name: 'Channels',
+        value: '' + this.bot.channels.cache.size,
+        inline: true,
+      }, {
+        name: 'Emojis',
+        value: '' + this.bot.emojis.cache.size,
+        inline: true,
+      }, {
+        name: 'Library',
+        value: 'discord.js',
+        inline: true,
+      }, {
+        name: 'Library Version',
+        value: `v${version}`,
+        inline: true,
+      }, {
+        name: 'Bot Created',
+        value: '' + this.bot.user.createdAt,
+        inline: true,
+      }, {
+        name: 'Node Version',
+        value: process.version,
+        inline: true,
+      }, {
+        name: 'NPM Version',
+        value: npmv.data.replace('\n', ''),
+        inline: true,
+      }, {
+        name: 'OS',
+        value: `${os.platform()} (${process.arch})`,
+        inline: true,
+      });
+
+    return interaction.editReply({ embeds: [embed] });
+  }
+};
