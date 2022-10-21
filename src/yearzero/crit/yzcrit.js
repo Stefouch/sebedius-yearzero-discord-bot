0e8d0d71e35bbdf1d8998c68a0e16adbf5a0f3fc
Stefouch <stefouch@gmail.com>
2022-10-21 16:33:44 +0200

    feat: ✨ YearZeroCrit

---
+++ src/yearzero/crit/yzcrit.js
@@ -0,0 +1,83 @@
+const { getBoolean } = require('../../utils/number-utils');
+
+/**
+ * A Year Zero Critical Injury object.
+ */
+class YearZeroCrit {
+  /**
+   * @param {Object}  data Critical data
+   * @param {number}  data.ref
+   * @param {string}  data.name
+   * @param {boolean} data.lethal
+   * @param {number}  data.healMalus
+   * @param {number}  data.timeLimit
+   * @param {string}  data.timeLimitUnit
+   * @param {string}  data.effect
+   * @param {number}  data.healingTime
+   * @throws {SyntaxError} If no data was given
+   */
+  constructor(data) {
+    if (!data) throw new SyntaxError('[YearZeroCrit] No data given to create this Crit!');
+
+    /**
+     * The reference in the Criticals table (D66).
+     * @type {number}
+     */
+    this.ref = data.ref || 0;
+
+    /**
+     * The name of the Critical Injury.
+     * @type {string}
+     */
+    this.name = data.name || '[❌ERROR:Crit_Name_Not_Found]';
+
+    /**
+     * Tells if the injury is lethal.
+     * @type {boolean}
+     */
+    this.lethal = getBoolean(data.lethal);
+
+    /**
+     * The malus to `HEAL` skill rolls.
+     * @type {number}
+     */
+    this.healMalus = +data.healMalus || 0;
+
+    /**
+     * The duration before you die from the injury.
+     * @see {@link YearZeroCrit.timeLimitUnit}
+     * @type {number}
+     */
+    this.timeLimit = +data.timeLimit || 0;
+
+    /**
+     * The units for the timeLimit duration.
+     * @see {@link YearZeroCrit.timeLimit}
+     * @type {string}
+     */
+    this.timeLimitUnit = data.timeLimitUnit || null;
+
+    /**
+     * The effects of the injury.
+     * @type {string}
+     */
+    this.effect = data.effect || '[❌ERROR:Crit_Effect_Not_Found]';
+
+    /**
+     * Healing time for the injury.
+     * @type {number}
+     */
+    this.healingTime = +data.healingTime || 0;
+  }
+
+  /**
+   * Tells if the Critical Injury is fatal (instant death).
+   * @type {boolean}
+   * @readonly
+   */
+  get fatal() {
+    return this.lethal && this.timeLimit === 0;
+  }
+}
+
+module.exports = YearZeroCrit;
