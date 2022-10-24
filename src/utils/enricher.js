95b432331e1e869b49cb5eb82722460d90ede227
Stefouch <stefouch@gmail.com>
2022-10-24 15:58:33 +0200

    feat(Client): ✨ Add text content enricher

---
+++ src/utils/enricher.js
@@ -0,0 +1,100 @@
+const YearZeroDie = require('../yearzero/roller/dice/yzdie');
+
+/**
+ * A class used to enrich text contents with replacement methods (called "enrichers").
+ */
+class TextEnricher {
+  constructor() {
+    /**
+     * List of available enrichers.
+     * @type {Enricher[]}
+     */
+    this.enrichers = [];
+
+    this.#setDefaultEnrichers();
+  }
+
+  /* ------------------------------------------ */
+
+  /**
+   * Adds new enrichers
+   * @param  {...Enricher} enrichers Enrichers to add
+   * @private
+   */
+  _addEnrichers(...enrichers) {
+    if (this.enrichers.length === 0) this.#setDefaultEnrichers();
+    this.enrichers.push(...enrichers);
+  }
+
+  #setDefaultEnrichers() {
+    this.enrichers = TextEnricher.DefaultEnrichers.slice();
+  }
+
+  /* ------------------------------------------ */
+  /*  Text Enricher                             */
+  /* ------------------------------------------ */
+
+  /**
+   * Enriches some text.
+   * @param {string}  text     Text to enrich
+   * @param {Object} [options] Options to add to the enricher method
+   */
+  async enrichText(text, options = {}) {
+    for (const { pattern, enricher } of this.enrichers) {
+      text = text.replace(pattern, (...args) => enricher(args, options));
+    }
+    return text;
+  }
+}
+
+/**
+ * @type {Enricher[]}
+ */
+TextEnricher.DefaultEnrichers = [
+  {
+    pattern: /\[\[(\d?)[dD](\d+)\]\]/gm,
+    enricher: rollEnricher,
+  },
+];
+
+module.exports = TextEnricher;
+
+/* ------------------------------------------ */
+/*  Default Enrichers Callbacks               */
+/* ------------------------------------------ */
+
+/**
+ * - $1: Number of dice
+ * - $2: Face of the di·c·e
+ */
+function rollEnricher(match, options) {
+  const title = match[0].replace(/\[|\]/g, '');
+  const qty = match[1] || 1;
+  const size = match[2] || 6;
+  let result = 0;
+  for (let i = 0; i < qty; i++) {
+    result += YearZeroDie.rng(1, size);
+  }
+  if (options.onlyNumber) {
+    return String(result);
+  }
+  // eslint-disable-next-line no-irregular-whitespace
+  return `🎲 __${title}: **${result}**__`;
+}
+
+/* ------------------------------------------ */
+/*  Types Definitions                         */
+/* ------------------------------------------ */
+
+/**
+ * @typedef {Object} Enricher
+ * @property {RegExp}           pattern
+ * @property {EnricherCallback} enricher
+ */
+
+/**
+ * @callback EnricherCallback
+ * @param {string[]} match
+ * @param {Object}  [options]
+ * @returns {string}
+ */
